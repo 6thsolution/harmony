b202019a53c939157d451c61d46659f457eec850
alireza <easazade@gmail.com>
2021-06-11 18:39:54 +0430

    feat(flutter) - now harmony generates open api module from configuration in pubspec.yaml

--- packages/harmony_network/harmony_network_cli/bin/main.dart
+++ packages/harmony_network/harmony_network_cli/bin/main.dart
@@ -1,19 +1,33 @@
 import 'dart:io';
 import 'dart:isolate';
 
+import 'package:harmony_network_cli/utils.dart';
+
 import 'constants.dart';
 
 void main(List<String> arguments) async {
+  print(' BOOOM !!!');
+  var config = loadYamlFileConfig('pubspec.yaml');
+  var inputFilePath = config['openapi_file_path'];
+  var outputDir = config['output_path'];
+  await generateModule(inputFilePath, outputDir);
+  print(config);
+}
+
+Future generateModule(String inputFilePath, String outputDir) async {
   exitCode = 0; // presume success
   var openApiJarUri = Uri.parse(OPENAPI_JAR_PATH);
   var binPath = (await Isolate.resolvePackageUri(openApiJarUri))!.toFilePath(windows: Platform.isWindows);
   var JAVA_OPTS = Platform.environment['JAVA_OPTS'] ?? '';
 
+  List<String> args = ['generate', '-i', inputFilePath, '-g', 'dart-dio-next', '-o', outputDir];
+
   var commands = [
     '-jar',
     "${"${binPath}"}",
-    ...arguments,
+    ...args,
   ];
+  print(commands);
   if (JAVA_OPTS.isNotEmpty) {
     commands.insert(0, JAVA_OPTS);
   }
